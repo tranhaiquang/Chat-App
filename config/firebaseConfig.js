afe141b9686d753814f40525dd92863705a1d65a
Quang Tran <haiquang1412@gmail.com>
2024-11-19 22:12:08 +0700

    load enviroment variable correctly for API

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -3,17 +3,17 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth, initializeAuth, getReactNativePersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-
+import { REACT_APP_FIREBASE_API } from "@env";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDF01u6ua4zd6nItt70xo5g8RAL-J5-YUU",
+  apiKey: REACT_APP_FIREBASE_API,
   authDomain: "authflow-97602.firebaseapp.com",
   projectId: "authflow-97602",
   storageBucket: "authflow-97602.appspot.com",
   messagingSenderId: "395516645635",
   appId: "1:395516645635:web:5ad60a9f7ba71fca4d1936",
-  measurementId: "G-02VK7G22D6"
+  measurementId: "G-02VK7G22D6",
 };
 
 // Initialize Firebase only if it hasn't been initialized yet
